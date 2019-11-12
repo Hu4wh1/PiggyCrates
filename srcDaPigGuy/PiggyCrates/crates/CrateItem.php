770e774dc73267d806c7461efeaa5c62cddc1509
Aldo <57144729+Hu4wh1@users.noreply.github.com>
2019-11-12 09:14:04 -0600

    Create CrateItem.php

---
+++ srcDaPigGuy/PiggyCrates/crates/CrateItem.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+namespace DaPigGuy\PiggyCrates\crates;
+use pocketmine\item\Item;
+/**
+ * Class CrateItem
+ * @package DaPigGuy\PiggyCrates\crates
+ */
+class CrateItem
+{
+    /** @var Item */
+    public $item;
+    /** @var string[] */
+    public $commands;
+    /** @var int */
+    public $chance;
+    /**
+     * CrateItem constructor.
+     * @param Item $item
+     * @param string[] $commands
+     * @param int $chance
+     */
+    public function __construct(Item $item, array $commands, int $chance)
+    {
+        $this->item = $item;
+        $this->commands = $commands;
+        $this->chance = $chance;
+    }
+    /**
+     * @return Item
+     */
+    public function getItem(): Item
+    {
+        return $this->item;
+    }
+    /**
+     * @return string[]
+     */
+    public function getCommands(): array
+    {
+        return $this->commands;
+    }
+    /**
+     * @return int
+     */
+    public function getChance(): int
+    {
+        return $this->chance;
+    }
+}
